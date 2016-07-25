73fb436f6f0b423a31a05e191e6764ec8fcd83bd
einsfr <einsfr@users.noreply.github.com>
2016-07-25 10:17:26 +0300

    Продолжение работы над #1 - Добавлена модель объекта в хранилище

--- mmkitstorage/models.py
+++ mmkitstorage/models.py
@@ -1,10 +1,53 @@
 import uuid
+import os
 
 from django.db import models
 from django.contrib.postgres.fields import HStoreField, ArrayField
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 from mmkitjournal.models import ActivityRecordableAbstractModel
+from mmkitcommon.utils import urlformatter, fs
+
+
+class StorageObject(models.Model):
+
+    class Meta:
+        verbose_name = 'объект в хранилище'
+        verbose_name_plural = 'объекты в хранилищах'
+        default_permissions = ()
+        unique_together = (('storage', 'path'), )
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    storage = models.ForeignKey(
+        'Storage',
+        related_name='stored_objects',
+        verbose_name=_('хранилище'),
+        help_text=_('Хранилище, в котором размещается объект')
+    )
+
+    path = models.CharField(
+        max_length=255,
+        verbose_name=_('путь к объекту'),
+        help_text=_('Путь к объекту относительно корня хранилища (не более 255 символов)')
+    )
+
+    def get_url(self, protocol: str) -> urlformatter.UrlFormatResult:
+        return self.storage.get_url(self, protocol)
+
+    def get_relative_path(self, inner_path: str = None) -> str:
+        if inner_path is None:
+            return os.path.normpath(self.path)
+        else:
+            return os.path.normpath(os.path.join(self.path, inner_path))
+
+    def get_absolute_path(self, inner_path: str = None) -> str:
+        return self.storage.get_object_path(self, inner_path)
 
 
 class Storage(ActivityRecordableAbstractModel):
@@ -59,11 +102,41 @@ class Storage(ActivityRecordableAbstractModel):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return  # TODO
 
     @staticmethod
-    def get_api_detail_view_name():
+    def get_api_detail_view_name() -> str:
         return  # TODO
 
+    def get_url(self, obj: StorageObject, protocol: str) -> urlformatter.UrlFormatResult:
+        try:
+            base_url = self.access_protocols[protocol]
+        except KeyError:
+            return None
+        return urlformatter.format_url('{}/{}'.format(base_url, obj.path)),
 
+    def get_base_path(self) -> str:
+        return os.path.normpath(os.path.join(
+            settings.MMKIT_STORAGES_ROOT_DIR,
+            self.base_dir
+        ))
+
+    def get_object_path(self, obj: StorageObject, inner_path: str = None) -> str:
+        if inner_path is None:
+            return os.path.normpath(os.path.join(
+                self.get_base_path(),
+                obj.path
+            ))
+        else:
+            return os.path.normpath(os.path.join(
+                self.get_base_path(),
+                obj.path
+            ))
+
+    def get_or_create_object(self, path: str) -> StorageObject:
+        try:
+            obj = self.stored_objects.get(path__iexact=path)
+        except StorageObject.DoesNotExist:
+            obj = StorageObject(path=path, storage=self).save()
+        return obj
