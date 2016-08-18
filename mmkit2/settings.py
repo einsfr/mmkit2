68ff9b28faadf8b57c845601e1fdf01b961f6936
einsfr <einsfr@users.noreply.github.com>
2016-08-18 21:53:46 +0300

    Исправлен список используемых приложений, а также создан файл со спецификацией API

--- mmkit2/settings.py
+++ mmkit2/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
+    'crispy_forms',
     'mmkitarchive',
     'mmkitcommon',
     'mmkitjournal',
